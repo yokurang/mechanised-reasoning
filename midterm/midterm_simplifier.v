598ae523527f87d9c9cfc9151b7c21f9fc81a0d3
ARandomPerson07 <chanadam0721@gmail.com>
2024-09-12 16:48:56 +0800

    typeful I think

--- midterm/midterm_simplifier.v
+++ midterm/midterm_simplifier.v
@@ -124,7 +124,7 @@ Qed.
 
 (* ********** *)
 
-Definition test_simplifier (candidate : arithmetic_expression -> arithmetic_expression) :=
+Definition test_simplifier_ltr (candidate : arithmetic_expression -> arithmetic_expression) :=
   let ae1 := Plus (Literal 1) (Literal 0) in
   let ae2 := Plus (Literal 0) (Literal 1) in
   let ae3 := Times (Literal 1) (Literal 2) in
@@ -138,20 +138,20 @@ Definition test_simplifier (candidate : arithmetic_expression -> arithmetic_expr
     (eqb_arithmetic_expression (candidate ae5) (Literal 0)) &&
     (eqb_arithmetic_expression (candidate ae6) (Literal 0)).
 
-Fixpoint simplifier (ae : arithmetic_expression) : arithmetic_expression :=
+Fixpoint simplifier_ltr (ae : arithmetic_expression) : arithmetic_expression :=
   match ae with
   | Literal n =>
       Literal n
   | Plus ae1 ae2 =>
       match ae1 with
       | Literal 0 =>
-          simplifier ae2
+          simplifier_ltr ae2
       | _ =>
           match ae2 with
           | Literal 0 =>
-              simplifier ae1
+              simplifier_ltr ae1
           | _ =>
-              Plus (simplifier ae1) (simplifier ae2)
+              Plus (simplifier_ltr ae1) (simplifier_ltr ae2)
           end
       end
   | Times ae1 ae2 =>
@@ -159,20 +159,190 @@ Fixpoint simplifier (ae : arithmetic_expression) : arithmetic_expression :=
       | Literal 0 =>
           Literal 0
       | Literal 1 =>
-          simplifier ae2
+          simplifier_ltr ae2
       | _ =>
           match ae2 with
           | Literal 0 =>
               Literal 0
           | Literal 1 =>
-              simplifier ae1
+              simplifier_ltr ae1
           | _ =>
-              Times (simplifier ae1) (simplifier ae2)
+              Times (simplifier_ltr ae1) (simplifier_ltr ae2)
           end
       end
   end.
 
-Compute (test_simplifier simplifier).
+Compute (test_simplifier_ltr simplifier_ltr).
+
+
+Lemma fold_unfold_simplifier_ltr_Literal :
+  forall n : nat,
+    simplifier_ltr (Literal n) =
+      Literal n.
+Proof.
+  fold_unfold_tactic evaluate_ltr.
+Qed.
+
+Lemma fold_unfold_simplifier_ltr_Plus :
+  forall ae1 ae2 : arithmetic_expression,
+    simplifier_ltr (Plus ae1 ae2) =
+      match ae1 with
+      | Literal 0 =>
+          simplifier_ltr ae2
+      | _ =>
+          match ae2 with
+          | Literal 0 =>
+              simplifier_ltr ae1
+          | _ =>
+              Plus (simplifier_ltr ae1) (simplifier_ltr ae2)
+          end
+      end.
+Proof.
+  fold_unfold_tactic simplifier_ltr.
+Qed.
+
+Lemma fold_unfold_simplifier_ltr_Times :
+  forall ae1 ae2 : arithmetic_expression,
+    simplifier_ltr (Times ae1 ae2) =
+       match ae1 with
+      | Literal 0 =>
+          Literal 0
+      | Literal 1 =>
+          simplifier_ltr ae2
+      | _ =>
+          match ae2 with
+          | Literal 0 =>
+              Literal 0
+          | Literal 1 =>
+              simplifier_ltr ae1
+          | _ =>
+              Times (simplifier_ltr ae1) (simplifier_ltr ae2)
+          end
+      end.
+Proof.
+  fold_unfold_tactic simplifier_ltr.
+Qed.
+
+(* The naive implementation above does work.
+   However, in the proof, we will have to
+   consider ae1 as Literal 0, Literal 1, or anything else, and
+   consider ae2 as Literal 0, Literal 1, or anything else in each of the cases of ae1.
+ *)
+
+
+Inductive arithmetic_sum : Type :=
+| Z : arithmetic_sum
+| W : arithmetic_sum
+| Literal_sum : nat -> arithmetic_sum
+| Plus_sum : arithmetic_sum -> arithmetic_sum -> arithmetic_sum
+| Times_sum : arithmetic_sum -> arithmetic_sum -> arithmetic_sum. 
+Fixpoint sum_to_expression (ams : arithmetic_sum) : arithmetic_expression  :=
+  match ams with
+  | Z => Literal 0
+  | W => Literal 1
+  | Literal_sum n =>
+      Literal n
+  | Plus_sum as1 as2 =>
+      Plus (sum_to_expression as1) (sum_to_expression as2)
+  | Times_sum as1 as2 =>
+      Times (sum_to_expression as1) (sum_to_expression as2)
+  end.
+                                                      
+Fixpoint simplify (ae : arithmetic_expression) : arithmetic_sum :=
+  match ae with
+  | Literal n =>
+      match n with
+      | 0 => Z
+      | 1 => W
+      | _ => Literal_sum n
+      end
+  | Plus ae1 ae2 =>
+      match ae1 with
+      | Literal 0 =>
+          simplify ae2
+      | _ =>
+          match ae2 with
+          | Literal 0 =>
+              simplify ae1
+          | _ =>
+              Plus_sum (simplify ae1) (simplify ae2)
+          end
+      end
+  | Times ae1 ae2 =>
+      match ae1 with
+      | Literal 0 =>
+          Z
+      | Literal 1 =>
+          simplify ae2
+      | _ =>
+          match ae2 with
+          | Literal 0 =>
+              Z
+          | Literal 1 =>
+              simplify ae1
+          | _ =>
+              Times_sum (simplify ae1) (simplify ae2)
+          end
+      end
+  end.
+
+Fixpoint eqb_arithmetic_sum (as1 as2 : arithmetic_sum) :=
+  match as1 with
+  | Z =>
+      match as2 with
+      | Z => true
+      | _ => false
+      end
+  | W =>
+      match as2 with
+      | W => true
+      | _ => false
+      end
+  | Literal_sum n1 =>
+      match as2 with
+      | Literal_sum n2 =>
+          n1 =? n2
+      | _ =>
+          false
+      end
+  | Plus_sum as11 as12 =>
+      match as2 with
+      | Plus_sum as21 as22 =>
+          (eqb_arithmetic_sum as11 as21) && (eqb_arithmetic_sum as12 as22)
+      | _ =>
+          false
+      end
+  | Times_sum as11 as12 =>
+      match as2 with
+      | Times_sum as21 as22 =>
+          (eqb_arithmetic_sum as11 as21) && (eqb_arithmetic_sum as12 as22)
+      | _ =>
+          false
+      end
+  end.
+
+Compute (eqb_arithmetic_sum Z Z).
+Compute (eqb_arithmetic_sum W W).
+Compute (negb (eqb_arithmetic_sum Z W)).
+Compute (negb (eqb_arithmetic_sum Z (Literal_sum 4))).
+Compute (negb (eqb_arithmetic_sum (Literal_sum 2) (Plus_sum W W))).
+(* explain why this is correct *)
+
+Definition test_simplify (candidate : arithmetic_expression -> arithmetic_sum) :=
+  let ae1 := Plus (Literal 1) (Literal 0) in
+  let ae2 := Plus (Literal 0) (Literal 1) in
+  let ae3 := Times (Literal 1) (Literal 2) in
+  let ae4 := Times (Literal 2) (Literal 1) in
+  let ae5 := Times (Literal 2) (Literal 0) in
+  let ae6 := Times (Literal 0) (Literal 2) in
+  (eqb_arithmetic_sum (candidate ae1) W) &&
+    (eqb_arithmetic_sum (candidate ae2) W) &&
+    (eqb_arithmetic_sum (candidate ae3) (Literal_sum 2)) &&
+    (eqb_arithmetic_sum (candidate ae4) (Literal_sum 2)) &&
+    (eqb_arithmetic_sum (candidate ae5) Z) &&
+    (eqb_arithmetic_sum (candidate ae6) Z).
+
+Compute (test_simplify simplify).
 
 (* ********** *)
 
