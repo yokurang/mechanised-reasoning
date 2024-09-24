4836d6e3cd9de6f7d6e894fe09b2c3677b1bef39
Vibilan Jayanth <vibilan@firstindo.com>
2024-09-24 20:34:33 +0800

    wk4: tried doing backward direction

--- week-04/week-04_another-conditional-equivalence.v
+++ week-04/week-04_another-conditional-equivalence.v
@@ -235,6 +235,87 @@ Proof.
     + reflexivity.
 Qed.
 
+Proposition Minus_is_conditionally_associative_sort_of_backward :
+  forall ae1 ae2 ae3 : arithmetic_expression,
+    evaluate_ltr (Minus (Minus ae1 ae2) ae3) =
+      evaluate_ltr (Minus ae1 (Plus ae2 ae3)) ->
+    (exists m1 : nat,
+        evaluate_ltr ae1 = Expressible_msg (Numerical_underflow m1))
+    \/
+      (exists m2 : nat,
+          evaluate_ltr ae2 = Expressible_msg (Numerical_underflow m2))
+    \/
+      (exists m3 : nat,
+          evaluate_ltr ae3 = Expressible_msg (Numerical_underflow m3)
+          /\
+            exists (n1 n2 : nat),
+              evaluate_ltr ae1 = Expressible_nat n1 ->
+              evaluate_ltr ae2 = Expressible_nat n2 ->
+              n2 <= n1 \/ n2 = n1 + m3)
+    \/
+      (exists n1 n2 : nat,
+          evaluate_ltr ae1 = Expressible_nat n1 ->
+          evaluate_ltr ae2 = Expressible_nat n2 ->
+          n2 <= n1 /\
+            (exists n3 : nat,
+                evaluate_ltr ae3 = Expressible_nat n3 ->
+                n2 + n3 <= n1)).
+Proof.
+  intros ae1 ae2 ae3 H.
+  rewrite ->3 fold_unfold_evaluate_ltr_Minus in H.
+  rewrite -> fold_unfold_evaluate_ltr_Plus in H.
+  case (evaluate_ltr ae1) as [n1 | m1] eqn:E_ae1.
+  - case (evaluate_ltr ae2) as [n2 | m2] eqn:E_ae2.
+    + case (evaluate_ltr ae3) as [n3 | m3] eqn:E_ae3.
+      * right; right; right.
+        exists n1, n2.
+        intros _ _.
+        split.
+        -- case (n1 <? n2) as [|] eqn:H_lt_n1_n2.
+           ++ Search (_ <? _).
+              Check (Nat.ltb_lt n1 n2).
+              destruct (Nat.ltb_lt n1 n2) as [lt_n1_n2 _].
+              Check (lt_n1_n2 H_lt_n1_n2).
+              Search ( _ < _ -> ~ _).
+              Check (Arith_prebase.lt_not_le_stt n1 n2).
+              Check (Arith_prebase.lt_not_le_stt n1 n2 (lt_n1_n2 H_lt_n1_n2)).
+              assert (not_lte_n2_n1 := (Arith_prebase.lt_not_le_stt n1 n2 (lt_n1_n2 H_lt_n1_n2))).
+              unfold not in not_lte_n2_n1.
+              admit. (* impossible *)
+           ++ apply (Nat.ltb_ge n1 n2).
+              exact H_lt_n1_n2.
+        -- exists n3.
+           intros _.
+           case (n1 <? n2 + n3) as [|] eqn:H_lt_n1_n2n3.
+           ++ apply (Nat.ltb_ge n1 (n2 + n3)).
+              admit. (* impossible *)
+           ++ apply (Nat.ltb_ge n1 (n2 + n3)).
+              exact H_lt_n1_n2n3.
+      * right; right; left.
+        case m3 as [].
+        exists n.
+        split.
+        -- reflexivity.
+        -- exists n1, n2.
+           intros _ _.
+           case (n1 <? n2) as [|] eqn:H_lt_n1_n2.
+           ++ right.
+              injection H as H.
+              rewrite <- H.
+              admit. (* possible but weird *)
+           ++ left.
+              apply (Nat.ltb_ge n1 n2).
+              exact H_lt_n1_n2.
+    + right; left.
+      case m2 as [].
+      exists n.
+      reflexivity.
+  - left.
+    case m1 as [].
+    exists n.
+    reflexivity.
+Qed.
+
 (* ********** *)
 
 (* end of week-04_another-conditional-equivalence.v *)
